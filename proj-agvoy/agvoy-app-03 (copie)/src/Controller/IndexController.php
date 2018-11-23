697ed20ed55ef205b1f10176bdc871abb76f952f
CHayé <hugochaye@hotmail.fr>
2018-11-23 12:19:12 +0100

    Version 4

---
+++ proj-agvoy/agvoy-app-03 (copie)/src/Controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Gestion de la page d'accueil de l'application
+ *
+ * @copyright  2017 Telecom SudParis
+ * @license    "MIT/X" License - cf. LICENSE file at project root
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controleur de la page d'accueil
+ */
+class IndexController extends Controller
+{    
+    /**
+     * @Route("/", name = "home", methods="GET")
+     */
+    public function indexAction()
+    {
+        return $this->render('home.html.twig',array(
+            'welcome' => "Bonjour, j'aime le chocolat",
+                )
+            );
+    }
+}
